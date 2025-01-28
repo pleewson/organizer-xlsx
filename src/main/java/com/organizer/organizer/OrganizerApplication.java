79cdfc34cc5841c35222dbede9c00b000197f611
pleewson <adrianplewait@gmail.com>
2025-01-28 12:40:21 +0100

    init

---
+++ src/main/java/com/organizer/organizer/OrganizerApplication.java
@@ -0,0 +1,14 @@
+package com.organizer.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizerApplication.class, args);
+		System.out.println("Hello organizer");
+	}
+
+}
